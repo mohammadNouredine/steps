149845ef71f461380e5dab575cc3943f451681db
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-26 20:17:43 +0300

    Update getDayRange.ts

--- client/src/backend/helpers/getDayRange.ts
+++ client/src/backend/helpers/getDayRange.ts
@@ -1,15 +1,30 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const TIMEZONE = "Asia/Beirut";
+
 function getDayRange(dateString: string) {
-  const date = new Date(dateString);
-  const startOfDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate()
-  );
-  const startOfNextDay = new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate() + 1
-  );
-  return { startOfDay, startOfNextDay };
+  // Parse the dateString **as Beirut time**
+  const date = dayjs.tz(dateString, TIMEZONE);
+
+  // Start of the day in Beirut TZ
+  const startOfDay = date.startOf("day").toDate();
+
+  // Start of the next day in Beirut TZ
+  const startOfNextDay = date.add(1, "day").startOf("day").toDate();
+
+  return {
+    startOfDay,
+    startOfNextDay,
+  };
 }
+
 export default getDayRange;
+
+export function transformDateFromUTCToTimezone(date: Date, timezone: string) {
+  return dayjs(date).tz(timezone).format("YYYY-MM-DD HH:mm:ss");
+}
