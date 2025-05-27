03f0027483d589ca004a3e75ef59c09a97ce993d
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-27 15:58:07 +0300

    Update formatDate.ts

--- client/src/helpers/formatDate.ts
+++ client/src/helpers/formatDate.ts
@@ -15,12 +15,13 @@ export const formatDateToDashes = (date: Date): string => {
 
 export function formatDateTime(date: Date) {
   if (!date) return null;
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
+  const day = String(d.getDate()).padStart(2, "0");
+  const hours = String(d.getHours()).padStart(2, "0");
+  const minutes = String(d.getMinutes()).padStart(2, "0");
+  const seconds = String(d.getSeconds()).padStart(2, "0");
 
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
