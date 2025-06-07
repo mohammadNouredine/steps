e2cf31ab82684bfa8f268bcbe173096de1c0e5bd
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-06-07 07:25:07 +0300

    Update delete-kid.service.ts

--- client/src/backend/kids/services/delete-kid.service.ts
+++ client/src/backend/kids/services/delete-kid.service.ts
@@ -1,10 +1,50 @@
+import { CustomErrorResponse } from "@/backend/helpers/customErrorResponse";
 import { deleteFromCloudinary } from "@/backend/lib/upload-utils";
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function deleteKid({ id }: { id: number }) {
-  const kidToBeDeleted = await prisma.kid.findFirst({ where: { id } });
+  const hasPayment = await prisma.payment.findFirst({
+    where: {
+      kidId: id,
+    },
+  });
 
+  if (hasPayment) {
+    return CustomErrorResponse("Kid has payments", 400);
+  }
+
+  const hasSubscription = await prisma.subscription.findFirst({
+    where: {
+      kidId: id,
+    },
+  });
+
+  if (hasSubscription) {
+    return CustomErrorResponse("Kid has subscription", 400);
+  }
+
+  const hasAttendance = await prisma.attendance.findFirst({
+    where: {
+      kidId: id,
+    },
+  });
+
+  if (hasAttendance) {
+    return CustomErrorResponse("Kid has attendance", 400);
+  }
+
+  const hasPurchasedItem = await prisma.purchasedItem.findFirst({
+    where: {
+      kidId: id,
+    },
+  });
+
+  if (hasPurchasedItem) {
+    return CustomErrorResponse("Kid has purchased items", 400);
+  }
+
+  const kidToBeDeleted = await prisma.kid.findFirst({ where: { id } });
   if (kidToBeDeleted?.image) {
     const delImage = await deleteFromCloudinary(kidToBeDeleted?.image);
     console.log("DEL IMAGE", delImage);
